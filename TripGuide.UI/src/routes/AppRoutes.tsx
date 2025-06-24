6a16702a38a7d51836f547ca2afcf15ac2ae6618
arinakapkovi4 <arinakapkovi4@gmail.com>
2025-06-24 17:41:06 +0300

    Auth 1.1

--- TripGuide.UI/src/routes/AppRoutes.tsx
+++ TripGuide.UI/src/routes/AppRoutes.tsx
@@ -1,15 +1,42 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import { Weather } from "../pages/Weather/Weather";
-import { Home } from "../pages/Home/Home";
-import { Introductory } from "../pages/Introductory/Introductory";
+// src/routes/AppRoutes.tsx
+import React from 'react';
+import { Routes, Route, Navigate } from 'react-router-dom';
+import { Home } from '../pages/Home/Home';
+import { Weather } from '../pages/Weather/Weather';
+import { Introductory } from '../pages/Introductory/Introductory';
+import Login from '../pages/LoginPage';
+import { NotFound } from '../pages/NotFound';
 
-const AppRoutes: React.FC = () => (
-  <Routes>
-    <Route path="/" element={<Home />} />
-    <Route path="/weather" element={<Weather />} />
-    <Route path="/Introductory" element={<Introductory />} />
-  </Routes>
-);
+interface PrivateRouteProps {
+  children: React.ReactElement;
+}
 
-export { AppRoutes };
+const PrivateRoute = ({ children }: PrivateRouteProps) => {
+  const isAuthenticated = localStorage.getItem('authToken') !== null;
+  return isAuthenticated ? children : <Navigate to="/login" replace />;
+};
+
+export const AppRoutes: React.FC = () => {
+  return (
+    <Routes>
+     
+      <Route path="/" element={<Home />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/introductory" element={<Introductory />} />
+
+
+      <Route
+        path="/weather"
+        element={
+          <PrivateRoute>
+            <Weather />
+          </PrivateRoute>
+        }
+      />
+
+
+      <Route path="*" element={<NotFound />} />
+    </Routes>
+  );
+};
+
