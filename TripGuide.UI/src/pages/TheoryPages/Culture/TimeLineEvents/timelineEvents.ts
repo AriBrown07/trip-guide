5f8e093c50a7a7a98e50d942b7e2811f1daabae0
Artem <art.petrovich13@gmail.com>
2025-08-04 18:18:02 +0300

    arrayes theory

---
+++ TripGuide.UI/src/pages/TheoryPages/Culture/TimeLineEvents/timelineEvents.ts
@@ -0,0 +1,274 @@
+export const timelineEvents = [
+    // {
+    //   letter: 'А',
+    //     events: [
+    //     {
+    //         title: "",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'Б',
+      events: [
+        {
+          title: "Богач",
+          link: "/bogach",
+        },
+      ],
+    },
+    {
+      letter: "В",
+      events: [
+        {
+          title: "Вербное воскресенье",
+          link: "/sunday",
+        },
+      ],
+    },
+    {
+      letter: 'Г',
+      events: [
+        {
+          title: "Громницы",
+          link: "/gromnitsy",
+        },
+      ],
+    },
+    {
+      letter: 'Д',
+      events: [
+        {
+          title: "Деды",
+          link: "/grandfathers",
+        },
+        {
+          title: "Драники",
+          link: "/draniki",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Е',
+    //     events: [
+    //     {
+    //         title: "Кревская уния",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ж',
+    //     events: [
+    //     {
+    //         title: "праздник урожая",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'З',
+      events: [
+        {
+          title: "Зажинки",
+          link: "/zazhinki",
+        },
+      ],
+    },
+    // {
+    //   letter: 'И',
+    //     events: [
+    //     {
+    //         title: "Принятие II Статута ВКЛ",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'К',
+      events: [
+        {
+          title: "Коляды",
+          link: "/kolyada",
+        },
+        {
+          title: "Купалье",
+          link: "/kupalle",
+        },
+      ],
+    },
+    {
+      letter: 'Л',
+      events: [
+        {
+          title: "Ляльник",
+          link: "/lyalnik",
+        },
+      ],
+    },
+    {
+      letter: 'М',
+      events: [
+        {
+          title: "Масленица",
+          link: "/carnival",
+        },
+      ],
+    },
+    {
+      letter: 'Н',
+      events: [
+        {
+          title: "Новый Год",
+          link: "/newYear",
+        },
+      ],
+    },
+    {
+      letter: 'О',
+      events: [
+        {
+          title: "День Октябрьской революции",
+          link: "/october",
+        },
+      ],
+    },
+    {
+      letter: 'П',
+      events: [
+        {
+          title: "Покров",
+          link: "/cover",
+        },
+        {
+          title: "Пасха",
+          link: "/easter",
+        },
+      ],
+    },
+    {
+      letter: 'Р',
+      events: [
+        {
+          title: "Радуница",
+          link: "/radunitsa",
+        },
+      ],
+    },
+    {
+      letter: 'С',
+      events: [
+        {
+          title: "Сорочины",
+          link: "/sorochiny",
+        },
+      ],
+    },
+    {
+      letter: 'Т',
+      events: [
+        {
+          title: "Толока",
+          link: "/cleanup",
+        },
+      ],
+    },
+    // {
+    //   letter: 'У',
+    //     events: [
+    //     {
+    //         title: "Формирование 'Громады'",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ф',
+    //     events: [
+    //     {
+    //         title: "Столыпинская аграрная реформа",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Х',
+    //     events: [
+    //     {
+    //         title: "Отречение Николая II от престола",
+    //         link: "/none",
+    //     },
+    //     {
+    //         title: "Октябрьская революция",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ц',
+    //     events: [
+    //     {
+    //         title: "Брест-Литовский договор",
+    //         link: "/none",
+    //     },
+    //     {
+    //         title: "Провозглашение БНР",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ч',
+    //     events: [
+    //     {
+    //         title: "Провозглашение БССР",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ш',
+    //     events: [
+    //     {
+    //         title: "Шчодры вечар",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'Щ',
+      events: [
+        {
+          title: "Щедрый вечер",
+          link: "/generous",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Э',
+    //     events: [
+    //     {
+    //         title: "Великая Отечественная война",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'Ю',
+      events: [
+        {
+          title: "Юрьев день",
+          link: "/yuriev",
+        },
+      ],
+    },
+    {
+      letter: 'Я',
+      events: [
+        {
+          title: "Яблочный Спас",
+          link: "/apple",
+        },
+      ],
+    },
+  ];
---
+++ TripGuide.UI/src/pages/TheoryPages/People/TimeLineEvents/timelineEvents.ts
@@ -0,0 +1,277 @@
+export const timelineEvents = [
+    {
+      letter: 'А',
+      events: [
+        {
+          title: "Жорес Алфёров",
+          link: "/none",
+        },
+        {
+          title: "Светлана Алексиевич",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Б',
+      events: [
+        {
+          title: "Симон Будный",
+          link: "/budny",
+        },
+        {
+          title: "Максим Богданович",
+          link: "/bogdanovich",
+        },
+      ],
+    },
+    {
+      letter: "В",
+      events: [
+        {
+          title: "Валентий Ванькович",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Г',
+      events: [
+        {
+          title: "Микола Гусовский",
+          link: "/gusovsky",
+        },
+      ],
+    },
+    {
+      letter: 'Д',
+      events: [
+        {
+          title: "Игнат Домейко",
+          link: "/none",
+        },
+        {
+          title: "Винцент Дунин-Марцинкевич",
+          link: "/none",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Е',
+    //     events: [
+    //     {
+    //         title: "Кревская уния",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'Ж',
+      events: [
+        {
+          title: "Дмитрий Жилунович",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'З',
+      events: [
+        {
+          title: "Михась Зарецкий",
+          link: "/none",
+        },
+      ],
+    },
+    // {
+    //   letter: 'И',
+    //     events: [
+    //     {
+    //         title: "Принятие II Статута ВКЛ",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'К',
+      events: [
+        {
+          title: "Якуб Колас",
+          link: "/kolas",
+        },
+        {
+          title: "Янка Купала",
+          link: "/kupala",
+        },
+        {
+          title: "Кастусь Калиновский",
+          link: "/kalinowski",
+        },
+        {
+          title: "Владимир Короткевич",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Л',
+      events: [
+        {
+          title: "Михась Лыньков",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'М',
+      events: [
+        {
+          title: "Петр Машеров",
+          link: "/none",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Н',
+    //     events: [
+    //     {
+    //         title: "Принятие Конституции Речи Посполитой",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'О',
+      events: [
+        {
+          title: "Наполеон Орда",
+          link: "/orda",
+        },
+      ],
+    },
+    {
+      letter: 'П',
+      events: [
+        {
+          title: "Ефросинья Полоцкая",
+          link: "/none",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Р',
+    //     events: [
+    //     {
+    //         title: "Третий раздел Речи Посполитой",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'С',
+      events: [
+        {
+          title: "Франциск Скорина",
+          link: "/skaryna",
+        },
+        {
+          title: "Михаил Савицкий",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Т',
+      events: [
+        {
+          title: "Тётка (Алоиза Пашкевич)",
+          link: "/none",
+        },
+        {
+          title: "Максим Танк",
+          link: "/tank",
+        },
+      ],
+    },
+    // {
+    //   letter: 'У',
+    //     events: [
+    //     {
+    //         title: "Формирование 'Громады'",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ф',
+    //     events: [
+    //     {
+    //         title: "Столыпинская аграрная реформа",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    {
+      letter: 'Х',
+      events: [
+        {
+          title: "Иван Хруцкий",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Ц',
+      events: [
+        {
+          title: "Виталий Цвирко",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Ч',
+      events: [
+        {
+          title: "Михась Чарот",
+          link: "/none",
+        },
+      ],
+    },
+    {
+      letter: 'Ш',
+      events: [
+        {
+          title: "Марк Шагал",
+          link: "/none",
+        },
+      ],
+    },
+    // {
+    //   letter: 'Э',
+    //     events: [
+    //     {
+    //         title: "Великая Отечественная война",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Ю',
+    //     events: [
+    //     {
+    //         title: "Вступление в ООН",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+    // {
+    //   letter: 'Я',
+    //     events: [
+    //     {
+    //         title: "Вступление в ЮНЕСКО",
+    //         link: "/none",
+    //     },
+    //     ],
+    // },
+  ];
